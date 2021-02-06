acde7bea395c8a437496dbcbd31a1ce8f811fcd6
Jawwad Hassan <jh@embrace-it.com>
2021-02-06 21:32:50 +0500

    feat: update addAsync and addAllAsync methods for map_node.dart

--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -21,7 +21,46 @@ Tree get mockTreeWithIds => Tree()
       ]),
   ]);
 
-MapNode get mockMapNode => MapNode("00")
+MapNode get mockMapNode1 => MapNode("M1")
+  ..addAll([
+    MapNode("0A")..add(MapNode("0A1A")),
+    MapNode("0B"),
+    MapNode("0C")
+      ..addAll([
+        MapNode("0C1A"),
+        MapNode("0C1B"),
+        MapNode("0C1C")
+          ..addAll([
+            MapNode("0C1C2A")
+              ..addAll([
+                MapNode("0C1C2A3A"),
+                MapNode("0C1C2A3B"),
+                MapNode("0C1C2A3C"),
+              ]),
+          ]),
+      ]),
+  ]);
+
+MapNode get mockMapNode2 => MapNode("M2")
+  ..addAll([
+    MapNode("0A")..add(MapNode("0A1A")),
+    MapNode("0B"),
+    MapNode("0C")
+      ..addAll([
+        MapNode("0C1A"),
+        MapNode("0C1B"),
+        MapNode("0C1C")
+          ..addAll([
+            MapNode("0C1C2A")
+              ..addAll([
+                MapNode("0C1C2A3A"),
+                MapNode("0C1C2A3B"),
+                MapNode("0C1C2A3C"),
+              ]),
+          ]),
+      ]),
+  ]);
+MapNode get mockMapNode3 => MapNode("M3")
   ..addAll([
     MapNode("0A")..add(MapNode("0A1A")),
     MapNode("0B"),
